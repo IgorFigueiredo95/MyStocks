8189ca7feb6c6404bfa375f3e56e204878e7d833
Igor A. Figueiredo <Igor998@gmail.com>
2024-03-20 23:14:07 -0300

    User entity

---
+++ MyStocks.Domain/Users/User.cs
@@ -0,0 +1,23 @@
+﻿using MyStocks.Domain.Primitives;
+using MyStocks.Domain.Users.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStocks.Domain.Users;
+
+public class User: Entity
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public Email Email { get; private set; }
+    public string Password { get; private set; }
+
+    private User()
+    {
+        
+    }
+
+}
---
+++ MyStocks.Domain/Users/ValueObjects/Email.cs
@@ -0,0 +1,42 @@
+﻿using MyStocks.Domain.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStocks.Domain.Users.ValueObjects;
+
+public class Email : ValueObject
+{
+    public string Address { get; private set; }
+
+    public bool IsValid { get; private set; }
+
+    private Email(string address)
+    {
+        Address = address;
+    }
+
+    public Email Create(string address)
+    {
+        if (Validate(address))
+            throw new ArgumentException($"The string '{address}' is an invalid email.");
+
+        Email email = new Email(address);
+        email.IsValid = true;
+
+        return email;
+    }
+
+    public static bool Validate(string address)
+    {
+       return  MailAddress.TryCreate(address, out var mailAddress);
+    }
+
+    public override List<object> GetAtomicValues()
+    {
+        return new List<object>() { Address };
+    }
+}
